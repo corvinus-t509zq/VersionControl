051083a223a7db5e142c254734ab772f10f2d8b1
Gergely Tanczos <gergelytanczos2000@gmail.com>
2021-09-29 16:22:02 +0200

    button click + listbox

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -7,17 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource2.LastName;
             label2.Text = Resource2.FisrtName;
             button1.Text = Resource2.Add;
+
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User();
+            u.FirstName = label2.Text;
+            u.LastName = label1.Text;
+            users.Add(u);
         }
     }
 }
