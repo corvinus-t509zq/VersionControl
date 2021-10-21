5485eea3d79c9f99706966b92f3abadd64572d8a
Gergely Tanczos <gergelytanczos2000@gmail.com>
2021-10-21 15:27:08 +0200

    file write

--- UserMaintenance/VaR/Form1.cs
+++ UserMaintenance/VaR/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using VaR.Entities;
+using System.IO;
 
 namespace VaR
 {
@@ -16,12 +17,31 @@ namespace VaR
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<decimal> Nyereségek = new List<decimal>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
+            
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            var nyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
         }
         void CreatePortfolio()
         {
@@ -43,6 +63,22 @@ namespace VaR
                 value += (decimal)last.Price * item.Volume;
             }
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK) {
+                StreamWriter sw = new StreamWriter(sfd.FileName);
+                sw.WriteLine("Időszak \t Nyereség");
+                for(int i = 1; i < Nyereségek.Count; i++)
+                {
+                    sw.WriteLine(i +"\t"+ Nyereségek[i]);
+                }
+
+                sw.Close();
+            }
+
         }
     }
     
