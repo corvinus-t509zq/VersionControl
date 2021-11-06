27d27a598708228e2827a06cbe54b736f12fc98c
Gergely Tanczos <gergelytanczos2000@gmail.com>
2021-11-06 12:24:54 +0100

    form1 módosítása az abszrakciókkal (Toy a ball helyett)

--- UserMaintenance/Mikulas/Form1.cs
+++ UserMaintenance/Mikulas/Form1.cs
@@ -1,4 +1,5 @@
-﻿using Mikulas.Entities;
+﻿using Mikulas.Abstractions;
+using Mikulas.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,9 +14,9 @@ namespace Mikulas
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
-        private BallFactory _factory;
-        public BallFactory Factory
+        private List<Toy> _toys = new List<Toy>();
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
             set { _factory = value; } 
@@ -30,18 +31,18 @@ namespace Mikulas
 
         private void CreateTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add(ball);
-            ball.Left = -ball.Width;
-            mainPanel.Controls.Add(ball);
+            var Toy = Factory.CreateNew();
+            _toys.Add(Toy);
+            Toy.Left = -Toy.Width;
+            mainPanel.Controls.Add(Toy);
         }
 
         private void ConveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxposition = 0;
-            foreach (var ball in _balls)
+            foreach (var ball in _toys)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxposition)
                 {
                     maxposition = ball.Left;
@@ -49,9 +50,9 @@ namespace Mikulas
             }
             if (maxposition > 1000)
             {
-                var oldestball = _balls[0];
+                var oldestball = _toys[0];
                 mainPanel.Controls.Remove(oldestball);
-                _balls.Remove(oldestball);
+                _toys.Remove(oldestball);
             }
         }
     }
