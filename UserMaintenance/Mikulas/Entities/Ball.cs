cf691b79ae10f2e3799ef1059827cb76704df681
Gergely Tanczos <gergelytanczos2000@gmail.com>
2021-11-06 11:49:00 +0100

    ball osztály módosítása

--- UserMaintenance/Mikulas/Entities/Ball.cs
+++ UserMaintenance/Mikulas/Entities/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mikulas.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -7,27 +8,12 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 namespace Mikulas.Entities
 {
-    public class Ball:Label
+    public class Ball : Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Width = 50;
-            Height = 50;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
-        public void MoveBall() 
-        {
-            Left += 1;
-        }
+
     }
 }
