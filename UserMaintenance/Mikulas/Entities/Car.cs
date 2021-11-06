c5dd012d42110267431daf813da9ede4a625b7a7
Gergely Tanczos <gergelytanczos2000@gmail.com>
2021-11-06 11:53:10 +0100

    car osztály + kép

---
+++ UserMaintenance/Mikulas/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using Mikulas.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikulas.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image ImageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(ImageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
