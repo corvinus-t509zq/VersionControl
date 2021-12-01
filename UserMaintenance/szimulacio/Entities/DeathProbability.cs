da4d35cfc88313ad229d026c79f22f8b4e449162
Gergely Tanczos <gergelytanczos2000@gmail.com>
2021-12-01 17:14:40 +0100

    osztályok létrehozása

---
+++ UserMaintenance/szimulacio/Entities/DeathProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szimulacio.Entities
+{
+    class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Age { get; set; }
+
+        public double Probability { get; set; }
+
+    }
+}
---
+++ UserMaintenance/szimulacio/Entities/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szimulacio.Entities
+{
+    public enum Gender
+    { 
+        Male = 1,
+        Femala = 2
+    }
+}
